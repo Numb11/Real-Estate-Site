b7ba6a1a801692de37b61600fe895a9c587d44e6
Soup <87945079+Numb11@users.noreply.github.com>
2024-05-01 15:24:23 +0100

    Web Development Project
    Real Estate site, used to enhance my web development skills

---
+++ script.js
@@ -0,0 +1,10 @@
+function openform(){
+    document.getElementById("EnquireSection").style.display = "block";
+    document.getElementById("html").style.opacity = "0.1";
+    document.getElementById("EnquireSection").style.opacity = "1";
+}
+
+function submit(){
+    document.getElementById("EnquireSection").style.display = "none";
+
+}
