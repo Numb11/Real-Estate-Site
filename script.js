4e37d152664dd40c69cdefe355baab1547029649
Soup <87945079+Numb11@users.noreply.github.com>
2024-06-12 13:07:29 +0100

    Finished

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+
+
 function openform(){
     document.getElementById("EnquireSection").style.display = "block";
     document.getElementById("html").style.opacity = "0.1";
     document.getElementById("EnquireSection").style.opacity = "1";
 }
 
-function submit(){
-    document.getElementById("EnquireSection").style.display = "none";
+function submitform(){
+    const info = document.getElementById("enquire");
+    let name = (info.elements["name"]).value;
+    let email = (info.elements["email"]).value;
+    let enquiry = (info.elements["enquiry"]).value;
+
+
+    emailjs.send("service_zko5yuh","template_hq7voyf",{
+        to_name: name,
+        message: enquiry,
+        email: email,
+        }, "_fC1jhZcfXnMvi2yl").then(
+            (response) => {
+                
+                alert("Enquiry logged!");
+            },
+            (error) => {
+                alert("Please retry");
+            },
+        );
+    
 
 }
